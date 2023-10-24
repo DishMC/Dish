d49a07c33284393807e96137b095e65ec18f2c7a
iris <60985521+KaiAF@users.noreply.github.com>
2023-10-24 12:32:30 -0500

    fix applying patches with no git repo initialized

--- patches/decompile-errors/applyPatches.js
+++ patches/decompile-errors/applyPatches.js
@@ -1,5 +1,7 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
+const path = require('path');
+
 const { error, log, warn } = require('../../config');
 
 const baseDir = __dirname;
@@ -10,20 +12,22 @@ function readDir(dir) {
   fs.readdirSync(dir).forEach(f => {
     try {
       if (fs.statSync(`${dir}/${f}`).isDirectory()) return readDir(`${dir}/${f}`);
-      fs.cpSync(`${dir}/${f}`, `decompiled/${f}`);
-      execSync(`cd decompiled && git apply ${f}`, { stdio: [] });
+      fs.cpSync(`${dir}/${f}`, `${path.join(baseDir, '../../decompiled')}/${f}`);
+      execSync(`cd ${path.join(baseDir, '../../decompiled')} && git apply ${f}`, { stdio: [process.stdin, process.stdout, process.stderr] });
       log(`Applied ${f.replace('.patch', '')}`);
     } catch (e) {
       rejected.push(f);
       error(`There was an error trying to apply a patch for '~/${f}'`);
       warn(e);
     }
-    fs.rmSync(`decompiled/${f}`);
+    fs.rmSync(`${path.join(baseDir, '../../decompiled')}/${f}`);
   });
 }
 
 (async function () {
+  // check if git repo is initialized, if not, it will not apply patches. 
+  if (!fs.existsSync('decompiled/.git')) return error('Git repo is not initialized. Run git init inside decompiled');
   readDir('patches/decompile-errors/decompiled/minecraft');
   readDir('patches/decompile-errors/decompiled/mojang');
-  if (rejected.length > 0) fs.writeFileSync('rejected_patches.rej', rejected.join('\n\n'));
+  if (rejected.length > 0) fs.writeFileSync('rejected_patches.rej', rejected.join('\n'));
 })();
