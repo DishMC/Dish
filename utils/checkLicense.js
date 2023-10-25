241389997dbe07a7c9120dbe1c7b705be23d82af
iris <60985521+KaiAF@users.noreply.github.com>
2023-10-24 21:30:49 -0500

    make checkLicense.js return error listing all files missing the headers [skip ci]

--- utils/checkLicense.js
+++ utils/checkLicense.js
@@ -8,21 +8,21 @@ const { error } = require('../config');
 
 const LICENSE_HEADER = fs.readFileSync('LICENSE-header.txt').toString();
 
-let MISSING_LICENSE = null;
+const MISSING_LICENSE = [];
 
 function readDir(dir) {
   fs.readdirSync(dir).forEach(f => {
     if (fs.statSync(`${dir}/${f}`).isDirectory()) return readDir(dir + '/' + f);
     if (!f.endsWith('.js')) return;
     const file = fs.readFileSync(`${dir}/${f}`).toString();
-    if (!file.startsWith(LICENSE_HEADER)) MISSING_LICENSE = dir + '/' + f;
+    if (!file.startsWith(LICENSE_HEADER)) MISSING_LICENSE.push(dir + '/' + f);
   });
 }
 
 (async function () {
   readDir('.');
-  if (MISSING_LICENSE) {
-    error(`Missing license header inside ${MISSING_LICENSE}`);
+  if (MISSING_LICENSE.length > 0) {
+    error(`Missing license header inside files: ${MISSING_LICENSE.join(', ')}`);
     return process.exit(1);
   }
   process.exit(0);
