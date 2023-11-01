dc26266a00e79a76cb7426a521f53bec9cdb9038
iris <60985521+KaiAF@users.noreply.github.com>
2023-11-01 15:28:38 -0500

    update default mc version

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const colors = require('./libs/colors');
 
-const DEFAULT_MINECRAFT_VERSION = "snapshot/23w43b";
+const DEFAULT_MINECRAFT_VERSION = "snapshot/23w44a";
 
 function log(msg) {
   console.log(`[Info]${colors.reset} ${msg}`);
