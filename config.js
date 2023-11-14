aedc0b9070d447ce5381eb102b13bb0ecdcc0313
iris <60985521+KaiAF@users.noreply.github.com>
2023-11-13 18:21:05 -0600

    update version

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const colors = require('./libs/colors');
 
-const DEFAULT_MINECRAFT_VERSION = "snapshot/23w45a";
+const DEFAULT_MINECRAFT_VERSION = "release/1.20.2";
 
 function log(msg) {
   console.log(`[Info]${colors.reset} ${msg}`);
